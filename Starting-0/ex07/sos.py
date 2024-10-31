1491e5c3bcca77739d8204ac6a42eead32de9607
khigl <khiglix@gmail.com>
2024-10-31 13:47:31 +0300

    ex07

---
+++ Starting-0/ex07/sos.py
@@ -0,0 +1,68 @@
+import sys
+
+
+NESTED_MORSE = {
+    " ": "/",
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "0": "-----",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----."
+}
+
+def doMorse(string: str):
+    """ Encode text into Morse """
+    morse = " "
+    text = [NESTED_MORSE[char.upper()] for char in string]
+    return morse.join(text)
+
+def main():
+    """ Main Function """
+    try:
+        number_of_args = len(sys.argv)
+        if number_of_args == 2:
+            string = sys.argv[1]
+            isAlphaNumeric = lambda a: a.isalpha() or a.isdigit()
+            for c in string:
+                if not isAlphaNumeric(c):
+                    raise ValueError("NG")
+            print(doMorse(string))
+        else:
+            print("AssertionError: the arguments are bad")
+    except ValueError:
+        print("AssertionError: the arguments are bad")
+
+
+if __name__ == "__main__":
+    main()
