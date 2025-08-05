ee05bf09f1a73b2447b676ed918753527a5a965a
higi <khiglix@gmail.com>
2025-08-05 19:23:22 +0300

    Array

---
+++ Array-1/ex00/give_bmi.py
@@ -0,0 +1,43 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) \
+     -> list[int | float]:
+    """
+        Get the list of heights and weights and calculate the BMI
+        for each value pair
+    """
+    try:
+        assert len(height) == len(weight), \
+            "Height and weight lists must be of the same length"
+    except AssertionError as e:
+        print(e)
+        exit(1)
+    bmi = []
+    for i in range(len(height)):
+        try:
+            assert isinstance(height[i], (int, float)) and \
+                isinstance(weight[i], (int, float)), \
+                "Height and weight must be numbers"
+            assert height[i] > 0 and weight[i] > 0, \
+                "Height and weight must be positive numbers"
+        except AssertionError as e:
+            print(e)
+            exit(1)
+        bmi.append(weight[i] / (height[i] ** 2))
+    return bmi
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+        This function takes a list of BMI values and a limit.
+        It returns a list of booleans,
+        True if the BMI is above the limit, False otherwise.
+    """
+    try:
+        assert isinstance(limit, int) and limit >= 0, \
+            "Limit must be a positive integer"
+        for i in bmi:
+            assert isinstance(i, (int, float)), \
+                "BMI must be a number"
+    except AssertionError as e:
+        print(e)
+        exit(1)
+    return [b > limit for b in bmi]
